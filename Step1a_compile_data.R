5654c959ef2b4b642cb39bdabbe7586ddfbd8aae
Ouzman <ouz001@csiro.au>
2025-12-23 14:54:10 +1030

    trying to make this more generic to capture all the data including GPS cood zone etc. I think the cleaning should be in a seperate step, ie part of the analysis

--- Step1a_compile_data.R
+++ Step1a_compile_data.R
@@ -14,6 +14,7 @@ library(broom)
 library(multcompView)
 library(lubridate)
 library(readxl)
+library(readr)
 
 ################################################################################
 ########################            Define the directory              ##########
@@ -23,10 +24,6 @@ dir <- "//fs1-cbr.nexus.csiro.au/{af-sandysoils-ii}"
 site_number <- "1.Walpeup_MRS125"
 site_name <- "Walpeup_MRS125"
 headDir <- paste0(dir, "/work/Output-1/", site_number)
-
-analysis.type <- "Emergence" #Harvest, InSeason, PeakBiomass, PreSeason
-variable <- "Total"
-treat.col.name <- "treat_desc"
 
 clean.dat <- "No"
 analysis.yr <- "25"
@@ -50,15 +47,6 @@ seasons <- readxl::read_excel(
   filter(Site == site_number)
 
 
-field_details <- readxl::read_excel(
-  paste0(metadata_path,metadata_file_name),
-  sheet = "location of observation data") %>% 
-  filter(Site == site_number) %>% 
-  filter(analysis_type == analysis.type )
-
-
-
-################################################################################
 
 
 
@@ -79,6 +67,35 @@ zones <- rast(paste0(file.path(headDir, file_path_details$`location of zone tif`
 zones <- terra::project(zones,paste0('epsg:',crs_used),method='near')
 
 
+################################################################################
+### Rinse and Repeat from here
+
+
+
+#Bring in the data for each field sampling event. These analysis.type are defined by Stirling
+
+#analysis.type <- "Emergence" #
+#analysis.type <- "Harvest" # No data yet
+
+#analysis.type <- "InSeason" ## ? not sure what this is
+analysis.type <- "PeakBiomass" #This is sometimes called biomass, or biomass at flowering
+# analysis.type <- "PreSeason" ## ? not sure what this is
+
+
+
+
+field_details <- readxl::read_excel(
+  paste0(metadata_path,metadata_file_name),
+  sheet = "location of observation data") %>% 
+  filter(Site == site_number) %>% 
+  filter(analysis_type == analysis.type )
+
+variable <- field_details$variable_clm_name
+treat.col.name <- "treat_desc"
+
+
+
+#################################################################################
 
 dat.raw <-   read_excel(file.path(headDir,  field_details$data))
 data.pts <-  st_read(file.path(headDir,   field_details$sampling_GPS))
@@ -87,9 +104,18 @@ data.pts_proj <- st_transform(data.pts, crs= crs_used)
 # data.pts_proj <- data.pts_proj %>% select(- c("pt_uuid", "site",
 #                                               "location", "treat_desc",
 #                                               "treat")) #remove some clm to help with the join
-data.pts_proj <- data.pts_proj %>% select("pt_id", "geometry" ) 
 
+
+################################################################################
+##################### STOP AND CHECK ###########################################
+
+### might need to stuff around here to makes sure the zone get imported
+names(data.pts_proj) 
+#data.pts_proj <- data.pts_proj %>% select("pt_id", "geometry" ) 
+data.pts_proj <- data.pts_proj %>% select("pt_id", "geometry", "cluster3","NDVI_drone" ) 
   
+
+
 names(dat.raw)
 names(data.pts_proj)
   
@@ -101,7 +127,7 @@ rm(dat.raw,data.pts, data.pts_proj, dat.all)
 
 ################################################################################
 data.raw <- data_sf #renaming data into generic name
-
+rm(data_sf)
 
 #assign the control when named vaguely
 
@@ -113,22 +139,101 @@ strips <- strips %>%
              .default = as.character(treat_desc)
            ) )
          
-         
-
+       
 
 
 
 ################################################################################
+## Add some clms AND Retain only a subset of clms
+
+str(data.raw)
+str(field_details)
+
+data.raw <- data.raw %>% 
+  mutate(site = site_name,
+         year = analysis.yr,
+         field_observation = analysis.type,
+         date_field_observation = field_details$Date_collected,
+         variable_name =  field_details$variable_name,
+         variable_units =  field_details$varibale_units
+         ) 
+
+names(data.raw)
 
 
+data.raw <- data.raw %>% 
+  select(site,
+         pt_id, treat, treat_desc, geometry,
+         year,
+         field_observation,
+         date_field_observation,   
+         !!variable, # Base tidyeval approach (unquoting)
+         # These are clms that might need modfiying
+         cluster3,
+         "NDVI_drone" )
 
-site.info <- list(
-  site_id    = site_name,
-  boundary   = boundary,      # sf
-  trial_plan = strips,    # sf
-  seasons    = seasons        # tibble
-)
-class(site.info) <- c("ssii_site", class(site.info))
+
+data.raw <-data.raw %>% rename("target.variable" = !!variable)
+
+str(data.raw)
+
+
+write_sf(data.raw,
+        paste0(headDir,"/10.Analysis/25/Processing_Jackie/", analysis.type, "_", analysis.yr,".shp"))
+
+rm( data.raw, analysis.type, variable , field_details)
+
+
+###############################################################################
+#### ONCE I get confirmation of all the variable this will grow.
+
+###############################################################################
+### Collate the variables into one file for the site
+analysis.yr <- 25
+data1 <- "Emergence"
+file1 <- st_read(paste0(headDir,"/10.Analysis/25/Processing_Jackie/", 
+                         data1, "_", analysis.yr,".shp"))  
+
+
+data2 <- "PeakBiomass"
+file2 <- st_read(paste0(headDir,"/10.Analysis/25/Processing_Jackie/", 
+                        data2, "_", analysis.yr,".shp"))  
+
+
+#### Getting the clm names matching
+names(file1)
+names(file2)
+
+file1 <- file1 %>%
+  rename("trgt_vr" = "Total" , "zone" = "clustr3") %>%
+  mutate(NDVI_drone = "nothing_yet")
+
+file2 <- file2 %>%
+  rename(
+         #"trgt_vr" = "Total" , 
+         "zone" = "clustr3",
+         "NDVI_drone" = "NDVI_dr" )
+
+file1_2 <- rbind(file1, file2)
+names(file1_2)
+
+file1_2 <- file1_2 %>%
+  rename(date = dt_fld_,
+        fld_ob = fld_bsr ) %>% 
+  mutate(date = ymd_hms(date))
+  
+  
+str(file1_2)
+###############################################################################
+########                     Lots of zeros               ######################
+###############################################################################
+
+file1_2 <- file1_2 %>%
+  mutate(across(c(trgt_vr), 
+                ~case_when(. == 0 ~ NA_real_,
+                           TRUE ~ .)))
+
+
 
 ###############################################################################
 ###############################################################################
