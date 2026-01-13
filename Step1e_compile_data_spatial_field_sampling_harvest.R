24702e2ff26338e67a6a936cfe31cfa0f824bcc3
Ouzman <ouz001@csiro.au>
2026-01-13 16:57:19 +1030

    harvest data started

---
+++ Step1e_compile_data_spatial_field_sampling_harvest.R
@@ -0,0 +1,80 @@
+## Stirling Roberton and modified by Jackie
+## Purpose: This script is built to undertake base analsysis of trial strip data
+
+## 1) get the field sampling location, date and results
+
+rm(list=ls())
+library(terra)
+library(sf)
+library(ggplot2)
+library(multcomp)
+library(dplyr)
+library(tidyr)
+library(broom)
+library(multcompView)
+library(lubridate)
+
+################################################################################
+########################            Define the directory              ##########
+################################################################################
+
+dir <- "//fs1-cbr.nexus.csiro.au/{af-sandysoils-ii}"
+site_number <- "1.Walpeup_MRS125"
+site_name <- "Walpeup_MRS125"
+headDir <- paste0(dir, "/work/Output-1/", site_number)
+
+
+
+
+clean.dat <- "No"
+analysis.yr <- "25"
+
+
+metadata_path <- paste0(dir,"/work/Output-1/0.Site-info/")
+metadata_file_name <- "names of treatments per site 2025 metadata and other info.xlsx"
+
+crs_used <- 4326
+################################################################################
+########################    Read in metadata info file names and path ##########
+################################################################################
+
+file_path_details <- readxl::read_excel(
+  paste0(metadata_path,metadata_file_name),
+  sheet = "location of file and details") %>% 
+  filter(Site == site_number)
+
+seasons <- readxl::read_excel(
+  paste0(metadata_path,metadata_file_name),
+  sheet = "seasons") %>% 
+  filter(Site == site_number)
+
+harvest_data_file <-  readxl::read_excel(
+  paste0(metadata_path,metadata_file_name),
+  sheet = "Harvest_data") %>% 
+  filter(Site == site_number)
+
+
+################################################################################
+## Read in field data with the Sential and drone data too
+
+
+collated_data_fld_Sent_drone <- st_read(paste0(headDir,
+                        "/10.Analysis/25/Processing_Jackie/", 
+                        site_name,"_collated_data_raw_Sen_drone_planet", 
+                        analysis.yr,".shp"))
+
+################################################################################
+################ Read in planet data ########################
+
+## get the list of files
+
+harvest_files <- list.files(path = paste0(headDir, "/", harvest_data_file$files), pattern = ".shp")
+harvest_files
+
+
+
+# Load the file 
+harvest_raw <- st_read(
+  paste0(headDir,"/", harvest_data_file$files,harvest_files))
+plot(harvest_raw)
+
