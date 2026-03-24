57f50e08cec42063401112ece00878445212d53a
Your Name <your.email@csiro.au>
2026-03-24 12:52:30 +1030

    adding orintation onto plots

--- Yield_Mointor_data/GUMS/2plot of yld with whole strip.R
+++ Yield_Mointor_data/GUMS/2plot of yld with whole strip.R
@@ -103,6 +103,39 @@ ggplot(combined, aes(x = Ladder_PointID*10, y = mean_yld, color = treat_desc)) +
   theme(legend.position = "none") +
   labs(title = "Yield down strip", x = "Distance along strip", y = "Mean yield")
 
+###############################################################################
+
+orientation_df <- read.csv(paste0(metadata_path, "ladder_orientation_all_sites.csv"))
+# Pull labels for current site
+start_label <- orientation_df$start_label[orientation_df$site == site_number]
+end_label   <- orientation_df$end_label[orientation_df$site == site_number]
+
+
+
+y_max <- max(combined$mean_yld, na.rm = TRUE)
+x_max <- max(combined$Ladder_PointID * 10, na.rm = TRUE)
+
+ggplot(combined, aes(x = Ladder_PointID * 10, y = mean_yld, color = treat_desc)) +
+  geom_line(linewidth = 1) +
+  facet_wrap(~ facet) +
+  scale_color_manual(values = treatment_colour) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  labs(title = "Yield down strip", 
+       # subtitle = "Control is grey",
+       caption = "Control is reference line in grey.",
+       x = "Distance along strip (m)", 
+       y = "Mean yield (t/ha)") +
+  annotate("text",
+           x = 0,
+           y = y_max,
+           label = start_label,
+           fontface = "bold",
+           size = 4,
+           hjust = 0,
+           vjust = 1) 
+
+
 headDir_analysis_folder <- paste0(headDir, "/10.Analysis/25/Harvest/")
 
 ggsave(paste0(headDir_analysis_folder,
